b86f1cbeba092a486609a57d18d6fe7cf730bafa
Yestin <y.vastenouw@gmail.com>
2019-09-27 12:07:04 +0200

    Update TournamentUserRoleController.php

--- app/Http/Controllers/TournamentUserRoleController.php
+++ app/Http/Controllers/TournamentUserRoleController.php
@@ -3,10 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Role;
-use App\Tournament;
 use App\TournamentUserRole;
 use Illuminate\Support\Facades\Auth;
-use Psy\Util\Json;
 
 class TournamentUserRoleController extends Controller
 {
