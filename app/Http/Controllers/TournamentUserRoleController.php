dc416b81fb9aaf54a21294de747e189762f971c3
Yestin <y.vastenouw@gmail.com>
2019-09-27 12:13:01 +0200

    Revert "Update TournamentUserRoleController.php"
    This reverts commit b86f1cbeba092a486609a57d18d6fe7cf730bafa.

--- app/Http/Controllers/TournamentUserRoleController.php
+++ app/Http/Controllers/TournamentUserRoleController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Role;
+use App\Tournament;
 use App\TournamentUserRole;
 use Illuminate\Support\Facades\Auth;
+use Psy\Util\Json;
 
 class TournamentUserRoleController extends Controller
 {
