885dbbf71430c5fd15dd155b4e9278498fa33a26
Bart Arentze <99046505@mydavinci.nl>
2019-10-08 13:15:22 +0200

    Fixed dashboard with widgets

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,29 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-
-@if (session('status'))
-    <div class="alert alert-success" role="alert">
-        <b>{{ session('status') }}</b>
-    </div>
-@endif
 <div class="container">
     <h1 class="text-center font-weight-bolder mb-4">Dashboard</h1>
     <div class="row">
         <div class="col-sm-4">
             <div class="card">
-                <h5 class="card-header"><b>Opkomende toernooien</b></h5>
+                <h4 class="card-header"><b>Toernooien</b></h4>
                 <div class="card-body">
-                    <p class="card-text"><b>Hier komen de opkomende toernooien waaraan je deelneemt</b></p>
-                    <a href="{{ url('tournament')}}" class="btn btn-primary">Ga naar toernooien</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-4">
-            <div class="card">
-                <h5 class="card-header"><b>Gespeelde toernooien</b></h5>
-                <div class="card-body">
-                    <p class="card-text"><b>Hier komen de toernooien waaraan je deelgenomen hebt</b></p>
+                    <table class="table table-borderless table-striped">
+                        <tbody>
+                        @foreach($tournaments as $tournament)
+                            <tr>
+                                <th>{{ $tournament->name }}</th>
+                                <td class="text-danger float-right">
+                                <a href="dashboard/{{ $tournament->id }}/leave" class="btn-link text-danger">Verlaat toernooi</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                     <a href="{{ url('tournament')}}" class="btn btn-primary">Ga naar toernooien</a>
                 </div>
             </div>
@@ -32,30 +28,10 @@
             <div class="card">
                 <h5 class="card-header"><b>Account-instellingen</b></h5>
                 <div class="card-body">
-                    <p class="card-text"><b>Hier komen de opkomende toernooien waaraan je deelneemt</b></p>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet iusto enim est sa</p>
                     <a href="{{ route('user.index', ['id' => auth()->user()->id]) }}" class="btn btn-primary">Ga naar account-instellingen</a>
                 </div>
             </div>
-        <div class="col-sm-12">
-            <table class="table">
-                <thead class="thead">
-                <tr>
-                    <th scope="col">Naam</th>
-                    <th scope="col">Beschrijving</th>
-                    <th scope="col"></th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($tournaments as $tournament)
-                    <tr>
-                        <th>{{ $tournament->name }}</th>
-                        <td>{{ $tournament->description }}</td>
-                        <td class="text-danger float-right"><a href="dashboard/{{ $tournament->id }}/leave" class="btn btn-danger">Verlaten</a>
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
         </div>
     </div>
 </div>
